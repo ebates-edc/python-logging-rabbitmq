d8503620b0278d2d780ee9a66dbbf0f590303c4e
Eran Kampf <eran@ekampf.com>
2018-06-17 08:59:43 -0700

    Upgarded to latest pika

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ CLASSIFIERS = [
 ]
 
 INSTALL_REQUIRES = [
-    'pika==0.10.0'
+    'pika==0.11.2'
 ]
 
 setup(name='python-logging-rabbitmq',
-      version='1.0.9',
+      version='1.0.10',
 
       url='https://github.com/albertomr86/python-logging-rabbitmq',
       description='Send logs to RabbitMQ from Python/Django',
